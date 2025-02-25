a6f6a3da33089568c7146ab60caf7bdba7d74728
Thiago Nunes Bizacha <bizacha@gmail.com>
2025-02-25 10:24:21 -0300

    Update load_postgresql.py

--- etl/load_postgresql.py
+++ etl/load_postgresql.py
@@ -18,10 +18,10 @@ logging.info(f"Extração iniciada")
 def load_to_postgresql(df):
     """Carrega os dados do DataFrame no banco de dados PostgreSQL."""
     conn = psycopg2.connect(
-        host="db-thiago-390438668051.cniie2kemucx.us-east-2.rds.amazonaws.com",
+        host="",
         database="postgres",
         user="postgres",
-        password="M3GFMwk3BteZfZyJVz4Y"
+        password=""
     )
     logging.info(f"Conexão com o banco de dados realizada com sucesso!")  
     print("Conexão com o banco de dados realizada com sucesso!")
@@ -54,10 +54,10 @@ def load_to_postgresql_bruto(df):
     """Carrega os dados do DataFrame no banco de dados bruto PostgreSQL."""
     # Conectando ao banco de dados
     conn = psycopg2.connect(
-        host="db-thiago-390438668051.cniie2kemucx.us-east-2.rds.amazonaws.com",
+        host="",
         database="postgres",
         user="postgres",
-        password="M3GFMwk3BteZfZyJVz4Y"
+        password=""
     )
     print("Conexão com o banco de dados extract_amazon_full realizada com sucesso!")
 
